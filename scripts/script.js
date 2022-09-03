9fd4555a10515343674339be22b4611d37d19cd6
abelkristv <abel.kristanto@gmail.com>
2022-09-03 23:55:28 +0700

    simplyfy javascript with jquerry

--- scripts/script.js
+++ scripts/script.js
@@ -1,39 +1,52 @@
-let header = document.querySelector("header")
-let content = document.getElementById("content")
-let form = document.getElementById('form')
-let project = document.getElementById("projects")
-let main = document.querySelector("main")
-console.log(main.innerHTML)
+// import "./jquery-3.6.1-min"
 
-let navbar = document.querySelector("nav")
+// let header = document.querySelector("header")
+// let content = document.getElementById("content")
+// let form = document.getElementById('form')
+// let project = document.getElementById("projects")
+// let main = document.querySelector("main")
+// console.log(main.innerHTML)
 
-function sticky() {
-    if (window.scrollY > navbar.offsetTop) {
-        navbar.style.position = "fixed"
-    } else {
-        navbar.style.position = "static"        
-    }
-}
+// let navbar = document.querySelector("nav")
 
-function display_form() {
-    // header.style.filter = "blur(8px)"
-    navbar.style.filter = "blur(8px)"
-    navbar.style.zIndex = 10
-    content.style.filter = "blur(8px)"
-    project.style.filter = "blur(8px)"
-    form.style.display = "block"
-    form.style.filter = "none"
-    form.style.postion = "fixed"
-}
+// function sticky() {
+//     if (window.scrollY > navbar.offsetTop) {
+//         navbar.style.position = "fixed"
+//     } else {
+//         navbar.style.position = "static"        
+//     }
+// }
 
-function remove_form() {
-    header.style.filter = "none"
-    content.style.filter = "none"
-    project.style.filter = "none" 
-    form.style.display = "none"   
-    navbar.style.filter = "none"
-}
+// function display_form() {
+//     // header.style.filter = "blur(8px)"
+//     navbar.style.filter = "blur(8px)"
+//     navbar.style.zIndex = 10
+//     content.style.filter = "blur(8px)"
+//     project.style.filter = "blur(8px)"
+//     form.style.display = "block"
+//     form.style.filter = "none"
+//     form.style.postion = "fixed"
+// }
 
-document.getElementById("contact").addEventListener("click", display_form)
-document.getElementById("form-close").addEventListener("click", remove_form)
-// window.onscroll = sticky
+// function remove_form() {
+//     header.style.filter = "none"
+//     content.style.filter = "none"
+//     project.style.filter = "none" 
+//     form.style.display = "none"   
+//     navbar.style.filter = "none"
+// }
+
+// document.getElementById("contact").addEventListener("click", display_form)
+// document.getElementById("form-close").addEventListener("click", remove_form)
+// // window.onscroll = sticky
+
+$(document).ready(function(){
+    $("#contact").click(function(){
+        $("#form").fadeIn()
+        $("main").css("filter", "blur(8px)")
+    })
+    $("#form-close").click(function(){
+        $("#form").fadeOut()
+        $("main").css("filter", "none")
+    })
+})
